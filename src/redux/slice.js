c37da51ba8397301bcd87970ec6d31a6dae82db3
loper14 <alisherovhamidullo2@gmail.com>
2023-02-28 09:55:26 +0500

    new changes

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -5,7 +5,7 @@ export let Slices = createSlice({
   name: "slices",
   initialState: {
     category: "House Plants",
-    minMax: [],
+    minMax: null,
     mapData: data,
   },
   reducers: {
@@ -14,15 +14,54 @@ export let Slices = createSlice({
     },
     setMinMax(state, action) {
       state.minMax = action.payload;
+      let filteredData = state.mapData;
+      filteredData[state.category].filter(
+        (value) => value.price > 10 && value.price < 100
+      );
+      state.mapData = filteredData;
     },
     setMapData(state, action) {
       state.mapData[state.category] = action.payload;
     },
-    sortMapData(state, action) {
+    sortDataBySale(state, action) {
       let copyData = state.mapData;
       copyData[state.category].sort((a, b) => (a.sale > b.sale ? -1 : 1));
+      state.mapData = copyData;
+    },
+    sortDataByNews(state, action) {
+      let copyData = state.mapData;
+      copyData[state.category].sort((a, b) =>
+        a.date.getTime() > b.date.getTime() ? -1 : 1
+      );
+      state.mapData = copyData;
+    },
+    sortDataByAll(state, action) {
+      state.mapData = data;
+    },
+    defaultSorting(state) {
+      state.mapData = data;
+    },
+    theCheapest(state) {
+      let copyData = state.mapData;
+      copyData[state.category].sort((a, b) => (a.price < b.price ? -1 : 1));
+      state.mapData = copyData;
+    },
+    mostExpensive(state) {
+      let copyData = state.mapData;
+      copyData[state.category].sort((a, b) => (a.price > b.price ? -1 : 1));
+      state.mapData = copyData;
     },
   },
 });
-export let { setCategory, setMinMax, setMapData, sortMapData } = Slices.actions;
+export let {
+  setCategory,
+  setMinMax,
+  setMapData,
+  sortDataBySale,
+  sortDataByNews,
+  sortDataByAll,
+  defaultSorting,
+  theCheapest,
+  mostExpensive,
+} = Slices.actions;
 export default Slices.reducer;
